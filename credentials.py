5e3137ffad7c569ab27f0e084a91850b82c2bb5d
wsarvesh <51471788+wsarvesh@users.noreply.github.com>
2019-09-25 22:42:07 +0530

    live tweet

---
+++ credentials.py
@@ -0,0 +1,6 @@
+# YouTube Video: https://www.youtube.com/watch?v=wlnx-7cm4Gg
+# Variables that contains the user credentials to access Twitter API 
+ACCESS_TOKEN = "1071763518452457472-hnCqhz5sOFVH1jsjoVxDnJQCyKbzhg"
+ACCESS_TOKEN_SECRET = "oTf4s3hwfJsTxZFknrBZ8UrR6tPsjfpIkS9YYOHzoc9iD"
+CONSUMER_KEY = "2JVagNOKjYSMvSMXa2tvqnZrs"
+CONSUMER_SECRET = "CMoo54u0XJwydvUWK3qH4COXU0UZwkNEWRVTUNAiX1mFCtAqvg"
