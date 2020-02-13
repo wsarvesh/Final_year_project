b695926dd70456b16ed615ca78f55bbe57f0aa35
wsarvesh <51471788+wsarvesh@users.noreply.github.com>
2020-02-13 23:42:11 +0530

    hj

--- credentials.py
+++ credentials.py
@@ -1,5 +1,4 @@
-# YouTube Video: https://www.youtube.com/watch?v=wlnx-7cm4Gg
-# Variables that contains the user credentials to access Twitter API 
+
 ACCESS_TOKEN = "1071763518452457472-hnCqhz5sOFVH1jsjoVxDnJQCyKbzhg"
 ACCESS_TOKEN_SECRET = "oTf4s3hwfJsTxZFknrBZ8UrR6tPsjfpIkS9YYOHzoc9iD"
 CONSUMER_KEY = "2JVagNOKjYSMvSMXa2tvqnZrs"
